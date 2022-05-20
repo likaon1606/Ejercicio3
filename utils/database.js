9509a988f2356cccd9a2fd1672c81a62c9480d01
Ariel <ariel.fuentesgarcia@gmail.com>
2022-05-20 13:53:16 -0500

    configuration

--- utils/database.js
+++ utils/database.js
@@ -10,6 +10,13 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   logging: false,
+  dialectOptions: process.env.NODE_ENV === 'production' ?  { 
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+  }, 
+}
+: {},
 });
 
 module.exports = { db };
